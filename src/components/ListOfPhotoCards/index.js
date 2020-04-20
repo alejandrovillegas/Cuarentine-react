13a5a1f76e5d7326baf2c8a1698b14413642594f
Alejandro Villegas <neopilatos@gmail.com>
2020-04-20 10:13:59 -0500

    localstorage hook

--- src/components/ListOfPhotoCards/index.js
+++ src/components/ListOfPhotoCards/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { PhotoCard } from '../PhotoCard';
 
 export const ListOfPhotoCards = () => {
-	return <ul>{[ 1, 2, 3, 4, 5, 6, 7, 8 ].map((id) => <PhotoCard key={id} />)}</ul>;
+	return <ul>{[ 1, 2, 3, 4, 5, 6, 7, 8 ].map((id) => <PhotoCard key={id} id={id} />)}</ul>;
 };
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,6 +1,6 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react';
 import { Article, ImgWrapper, Img, Button } from './styles';
-import { MdFavoriteBorder } from 'react-icons/md';
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
 
 const DEFAULT_IMAGE =
 	'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60';
@@ -8,6 +8,16 @@ const DEFAULT_IMAGE =
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 	const element = useRef(null);
 	const [ show, setShow ] = useState(false);
+	const key = `like-${id}`;
+
+	const [ liked, setLiked ] = useState(() => {
+		try {
+			const like = window.localStorage.getItem(key);
+			return like;
+		} catch (e) {
+			return false;
+		}
+	});
 
 	useEffect(
 		function() {
@@ -29,6 +39,17 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 		[ element ]
 	);
 
+	const Icon = liked ? MdFavorite : MdFavoriteBorder;
+
+	const setLocalStorage = (value) => {
+		try {
+			window.localStorage.setItem(key, value);
+			setLiked(value);
+		} catch (e) {
+			console.error(e);
+		}
+	};
+
 	return (
 		<Article ref={element}>
 			{show && (
@@ -39,8 +60,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
 						</ImgWrapper>
 					</a>
 
-					<Button>
-						<MdFavoriteBorder size="32px" /> {likes} likes!
+					<Button onClick={() => setLocalStorage(!liked)}>
+						<Icon size="32px" /> {likes} likes!
 					</Button>
 				</Fragment>
 			)}
