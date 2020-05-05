2c1c60608dea1b8fefba553ca5747c81dcf0c27a
Alejandro Villegas <neopilatos@gmail.com>
2020-05-05 18:21:07 -0500

    reach router

--- src/components/Category/styles.js
+++ src/components/Category/styles.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
+import { Link as LinkRouter } from '@reach/router';
 
-export const Anchor = styled.a`
-	display: flex;
-	flex-direction: column;
-	text-align: center;
-	text-decoration: none;
-	width: 75px;
+export const Link = styled(LinkRouter)`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  text-decoration: none;
+  width: 75px;
 `;
 
 export const Image = styled.img`
